7c828436604199e2c67d4256fe3b6eaa848bb283
Christian Schroeder <cs@robots.ox.ac.uk>
2020-07-15 18:55:57 +0100

    fixed rl_games dockerfile cuda version

--- launch_servers.sh
+++ launch_servers.sh
@@ -4,11 +4,11 @@
 #      ./run_servers.sh 3 python3 torch_runner.py --file=whirl_baselines/3m_torch with name=test3_3m_torch label=test3_3m_torch &
 #done
 
-for i in {1..5}; do 
+for i in {1..3}; do 
       #./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/3m_torch_cnn with name=test3_3m_torch_cnn label=test3_3m_torch_cnn &
       #./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/2s3z_torch_cnn with name=test3_2s3z_torch_cnn label=test3_2s3z_torch_cnn &
       #./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/2s3z_torch with name=test3_2s3z_torch label=test3_2s3z_torch &
       #./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/5m_vs_6m_torch with name=test3_5m_vs_6m_torch label=test3_5m_vs_6m_torch &
       #./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/5m_vs_6m_torch_cnn with name=test3_5m_vs_6m_torch_cnn label=test3_5m_vs_6m_torch_cnn &
-      ./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/corridor_torch with name=corridor_torch label=corridor_torch &
+      ./run_servers.sh ${i} python3 tf14_runner.py --file=whirl_baselines/3s_vs_5z with name=3s_vs_5z_tf label=3s_vs_5z_tf &
 done
