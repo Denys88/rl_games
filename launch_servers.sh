31c08ab840af5c910711c1fc106cdd23ce123219
Your Name <you@example.com>
2020-07-15 17:56:40 +0000

    minor

--- launch_servers.sh
+++ launch_servers.sh
@@ -10,5 +10,7 @@ for i in {1..5}; do
       #./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/2s3z_torch with name=test3_2s3z_torch label=test3_2s3z_torch &
       #./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/5m_vs_6m_torch with name=test3_5m_vs_6m_torch label=test3_5m_vs_6m_torch &
       #./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/5m_vs_6m_torch_cnn with name=test3_5m_vs_6m_torch_cnn label=test3_5m_vs_6m_torch_cnn &
-      ./run_servers.sh ${i} python3 torch_runner.py --file=whirl_baselines/corridor_torch with name=corridor_torch label=corridor_torch &
+      #./run_servers.sh ${i} python3 tf14_runner.py --file=whirl_baselines/corridor with name=corridor_tf label=corridor_tf &
+      # ./run_servers.sh ${i} python3 tf14_runner.py --file=whirl_baselines/27m_vs_30m with name=27m_vs_30m_tf label=27m_vs_30m_tf &
+      ./run_servers.sh ${i} python3 tf14_runner.py --file=whirl_baselines/vdn_MMM2 with name=vdn_MMM2_tf label=vdn_MMM2_tf & 
 done
