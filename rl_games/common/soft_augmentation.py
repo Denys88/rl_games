d4b8a53a44fc7783ee158e388be1ba62206495dc
Denys88 <trrrrr97@gmail.com>
2020-12-25 15:03:35 -0800

    create sa class

---
+++ rl_games/common/soft_augmentation.py
@@ -0,0 +1,15 @@
+import kornia
+
+
+class SoftAugmentation():
+    def __init__(self):
+        pass
+
+    def augment_batch(self):
+        pass
+
+    def value_loss(self):
+        pass
+
+    def policy_loss(self):
+        pass
