28b709b2ce4b2da258ab2bd314b58eb83edded54
Aleksei Petrenko <petrenko@usc.edu>
2021-09-15 17:35:57 -0700

    Moved IVecEnv

--- rl_games/common/ivecenv.py
+++ rl_games/common/ivecenv.py
@@ -1,9 +1,9 @@
-class IVecEnv(object):
+class IVecEnv:
     def step(self, actions):
-        raise NotImplementedError 
+        raise NotImplementedError
 
     def reset(self):
-        raise NotImplementedError    
+        raise NotImplementedError
 
     def has_action_masks(self):
         return False
@@ -12,4 +12,22 @@ class IVecEnv(object):
         return 1
 
     def get_env_info(self):
+        pass
+
+    def set_train_info(self, env_frames, *args, **kwargs):
+        """
+        Send the information in the direction algo->environment.
+        Most common use case: tell the environment how far along we are in the training process. This is useful
+        for implementing curriculums and things such as that.
+        """
+        pass
+
+    def get_env_state(self):
+        """
+        Return serializable environment state to be saved to checkpoint.
+        Can be used for stateful training sessions, i.e. with adaptive curriculums.
+        """
+        return None
+
+    def set_env_state(self, env_state):
         pass
--- rl_games/common/vecenv.py
+++ rl_games/common/vecenv.py
@@ -8,41 +8,6 @@ import gym
 from time import sleep
 
 
-class IVecEnv:
-    def step(self, actions):
-        raise NotImplementedError 
-
-    def reset(self):
-        raise NotImplementedError    
-
-    def has_action_masks(self):
-        return False
-
-    def get_number_of_agents(self):
-        return 1
-
-    def get_env_info(self):
-        pass
-
-    def set_train_info(self, env_frames, *args, **kwargs):
-        """
-        Send the information in the direction algo->environment.
-        Most common use case: tell the environment how far along we are in the training process. This is useful
-        for implementing curriculums and things such as that.
-        """
-        pass
-
-    def get_env_state(self):
-        """
-        Return serializable environment state to be saved to checkpoint.
-        Can be used for stateful training sessions, i.e. with adaptive curriculums.
-        """
-        return None
-
-    def set_env_state(self, env_state):
-        pass
-
-
 class RayWorker:
     def __init__(self, config_name, config):
         self.env = configurations[config_name]['env_creator'](**config)
