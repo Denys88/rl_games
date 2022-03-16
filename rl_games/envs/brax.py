aa298abb264bb58ef816561cf6f0976ed2512d2c
ViktorM <victor.makoviychuk@gmail.com>
2022-03-16 13:57:55 -0700

    Brax notebook update.

--- rl_games/envs/brax.py
+++ rl_games/envs/brax.py
@@ -1,9 +1,6 @@
-
-
 from rl_games.common.ivecenv import IVecEnv
 import gym
 import numpy as np
-import torch
 import torch.utils.dlpack as tpack
 
 def jax_to_torch(tensor):
@@ -21,10 +18,7 @@ def torch_to_jax(tensor):
 
 class BraxEnv(IVecEnv):
     def __init__(self, config_name, num_actors, **kwargs):
-        import brax
         from brax import envs    
-        import jax
-        import jax.numpy as jnp
 
         self.batch_size = num_actors
         env_name=kwargs.pop('env_name', 'ant')
@@ -40,7 +34,6 @@ class BraxEnv(IVecEnv):
         action_high = np.ones(self.env._env.unwrapped.action_size)
         self.action_space = gym.spaces.Box(-action_high, action_high, dtype=np.float32)
 
-
     def step(self, action):
         action = torch_to_jax(action)
         next_obs, reward, is_done, info = self.env.step(action)
@@ -50,8 +43,6 @@ class BraxEnv(IVecEnv):
         return next_obs, reward, is_done, info
 
     def reset(self):
-        import jax
-        rng = jax.random.PRNGKey(seed=0)
         obs = self.env.reset()
         return jax_to_torch(obs)
 
