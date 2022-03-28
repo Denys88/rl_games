af9377d94dc187be63c313495ee0e9b4aac2b3fe
Arthur Allshire <aallshire@nvidia.com>
2022-03-28 14:11:43 -0400

    fix multi gpu with CV

--- rl_games/algos_torch/a2c_discrete.py
+++ rl_games/algos_torch/a2c_discrete.py
@@ -58,7 +58,8 @@ class DiscreteA2CAgent(a2c_common.DiscreteA2CBase):
                 'config' : self.central_value_config, 
                 'writter' : self.writer,
                 'max_epochs' : self.max_epochs,
-                'multi_gpu' : self.multi_gpu
+                'multi_gpu' : self.multi_gpu,
+                'hvd': self.hvd if self.multi_gpu else None
             }
             self.central_value_net = central_value.CentralValueTrain(**cv_config).to(self.ppo_device)
 
