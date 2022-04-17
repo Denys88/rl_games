dba3a0f5dd5582982781f12777f49b61fa5d09a5
Denys Makoviichuk <denis_valerievich@yahoo.com>
2022-04-16 17:18:31 -0700

    Added atari scores to the envpool (#140)
    * fixed scores
    
    * best config
    
    * configs update
    
    * fixed envpool pong scores
    
    Co-authored-by: Viktor Makoviichuk <trrrrr97@gmail.com>

--- rl_games/envs/envpool.py
+++ rl_games/envs/envpool.py
@@ -9,6 +9,7 @@ class Envpool(IVecEnv):
 
         self.batch_size = num_actors
         env_name=kwargs.pop('env_name')
+        self.has_lives = kwargs.pop('has_lives', False)
         self.env = envpool.make( env_name,
                                  env_type=kwargs.pop('env_type', 'gym'),
                                  num_envs=num_actors,
@@ -20,11 +21,28 @@ class Envpool(IVecEnv):
         self.observation_space = self.env.observation_space
         self.ids = np.arange(0, num_actors)
         self.action_space = self.env.action_space
-        #self.scores = np.zeros(num_actors)
+        self.scores = np.zeros(num_actors)
+        self.returned_scores = np.zeros(num_actors)
+
+    def _set_scores(self, infos, dones):
+        # thanks to cleanrl: https://github.com/vwxyzjn/cleanrl/blob/3d20d11f45a5f1d764934e9851b816d0b03d2d10/cleanrl/ppo_atari_envpool.py#L111
+        self.scores += infos["reward"]
+        self.returned_scores[:] = self.scores
+        infos["scores"] = self.returned_scores
+
+        if self.has_lives:
+            all_lives_exhausted = infos["lives"] == 0
+            self.scores *= 1 - all_lives_exhausted
+        else:
+            # removing lives otherwise default observer will use them
+            if 'lives' in infos:
+                del infos['lives']
+            self.scores *= 1 - dones
 
     def step(self, action):
         next_obs, reward, is_done, info = self.env.step(action , self.ids)
         info['time_outs'] = info['TimeLimit.truncated']
+        self._set_scores(info, is_done)
         return next_obs, reward, is_done, info
 
     def reset(self):
