e6afb5fc3f4157789f930e5b3284354cc3af9706
ViktorM <victor.makoviychuk@gmail.com>
2023-04-02 15:11:39 -0700

    Fix.

--- rl_games/common/a2c_common.py
+++ rl_games/common/a2c_common.py
@@ -1312,8 +1312,6 @@ class ContinuousA2CBase(A2CBase):
                 should_exit_t = torch.tensor(should_exit, device=self.device).float()
                 dist.broadcast(should_exit_t, 0)
                 should_exit = should_exit_t.float().item()
-            if should_exit:
-                return self.last_mean_rewards, epoch_num
 
             if should_exit:
                 return self.last_mean_rewards, epoch_num
