17ae37867cfcd988c3f3af8ca8d601015f8c18b9
ViktorM <victor.makoviychuk@gmail.com>
2023-04-02 21:50:45 -0700

    Updated to the latest Brax APIs.

--- rl_games/envs/brax.py
+++ rl_games/envs/brax.py
@@ -18,10 +18,11 @@ def torch_to_jax(tensor):
 
 class BraxEnv(IVecEnv):
     def __init__(self, config_name, num_actors, **kwargs):
+        import jax
         from brax import envs
     #    import jax.numpy as jnp
 
-        self.batch_size = num_actors
+        self.num_envs = num_actors
         self.env_name = kwargs.pop('env_name', 'ant')
         self.sim_backend = kwargs.pop('backend', 'positional') # can be 'generalized', 'positional', 'spring'
         # self.env = envs.create_gym_env(env_name=self.env_name,
@@ -30,27 +31,37 @@ class BraxEnv(IVecEnv):
         #            backend = 'gpu'
         #            )
         
-        self.env = envs.get_environment(env_name=self.env_name, backend=self.sim_backend)
+        #self.env = envs.get_environment(env_name=self.env_name, backend=self.sim_backend)
 
-        obs_high = np.inf * np.ones(self.env._env.unwrapped.observation_size)
+        self.env = envs.create(env_name=self.env_name, batch_size=self.num_envs, backend=self.sim_backend)
+                                # env_name: str,
+                                # episode_length: int = 1000,
+                                # action_repeat: int = 1,
+                                # auto_reset: bool = True,
+                                # batch_size: Optional[int] = None,
+                                # **kwargs)
+
+        self.state = self.env.reset(rng=jax.random.PRNGKey(self.seed))
+
+        obs_high = np.inf * np.ones(self.env.observation_size)
         self.observation_space = gym.spaces.Box(-obs_high, obs_high, dtype=np.float32)
 
-        action_high = np.ones(self.env._env.unwrapped.action_size)
+        action_high = np.ones(self.env.action_size)
         self.action_space = gym.spaces.Box(-action_high, action_high, dtype=np.float32)
 
     def step(self, action):
         action = torch_to_jax(action)
-        next_obs, reward, is_done, info = self.env.step(action)
-        next_obs = jax_to_torch(next_obs)
-        reward = jax_to_torch(reward)
-        is_done = jax_to_torch(is_done)
+        self.state = self.env.step(self.state, action)
+        next_obs = jax_to_torch(self.state.obs)
+        reward = jax_to_torch(self.state.reward)
+        is_done = jax_to_torch(self.state.done)
 
-        return next_obs, reward, is_done, info
+        return next_obs, reward, is_done, self.state.info
 
     def reset(self):
-        # todo add random init like in collab examples?
-        obs = self.env.reset()
-        return jax_to_torch(obs)
+        self.state = self.env.reset(rng=jax.random.PRNGKey(self.seed))
+
+        return jax_to_torch(self.state.obs)
 
     def get_number_of_agents(self):
         return 1
