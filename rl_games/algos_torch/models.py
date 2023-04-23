cef05e4bad918d27afc5fdb3b5da4719b63a376f
Noname <nepalimsa@>
2023-04-23 11:35:33 -0400

    added ppo-ue

--- rl_games/algos_torch/models.py
+++ rl_games/algos_torch/models.py
@@ -276,6 +276,9 @@ class ModelA2CContinuousLogStd(BaseModel):
                 return result
             else:
                 selected_action = distr.sample()
+                choice = torch.rand_like(selected_action) > 0.02
+                choice = choice.float()
+                selected_action = selected_action * choice + mu * (1 - choice)
                 neglogp = self.neglogp(selected_action, mu, sigma, logstd)
                 result = {
                     'neglogpacs' : torch.squeeze(neglogp),
