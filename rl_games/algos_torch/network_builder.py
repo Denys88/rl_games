3531fb881d6ec7f24953852865bc38457cf6c9e1
ViktorM <victor.makoviychuk@gmail.com>
2024-08-22 13:18:30 -0700

    Pacman training.

--- rl_games/algos_torch/network_builder.py
+++ rl_games/algos_torch/network_builder.py
@@ -1098,6 +1098,8 @@ class VisionBackboneBuilder(NetworkBuilder):
             self.actions_num = kwargs.pop('actions_num')
             full_input_shape = kwargs.pop('input_shape')
 
+            print("Observations shape: ", full_input_shape)
+
             self.proprio_size = 0 # Number of proprioceptive features
             if isinstance(full_input_shape, dict):
                 input_shape = full_input_shape['camera']
@@ -1116,7 +1118,6 @@ class VisionBackboneBuilder(NetworkBuilder):
 
             self.cnn, self.cnn_output_size = self._build_backbone(input_shape, params['backbone'])
 
-            self.resize_transform = transforms.Resize((224, 224))
             mlp_input_size = self.cnn_output_size + self.proprio_size
             if len(self.units) == 0:
                 out_size = self.cnn_output_size
@@ -1287,10 +1288,13 @@ class VisionBackboneBuilder(NetworkBuilder):
             pretrained = backbone_params.get('pretrained', False)
             self.preprocess_image = backbone_params.get('preprocess_image', False)
 
-            if backbone_type == 'resnet18':
-                backbone = models.resnet18(pretrained=pretrained, zero_init_residual=True) # norm_layer=nn.LayerNorm
-                # Modify the first convolution layer to match input shape if needed
+            if backbone_type == 'resnet18' or backbone_type == 'resnet34':
+                if backbone_type == 'resnet18':
+                    backbone = models.resnet18(pretrained=pretrained, zero_init_residual=True)
+                else:
+                    backbone = models.resnet34(pretrained=pretrained, zero_init_residual=True)
 
+                # Modify the first convolution layer to match input shape if needed
                 # TODO: add low-res parameter
                 backbone.conv1 = nn.Conv2d(input_shape[0], 64, kernel_size=3, stride=1, padding=1, bias=False)
                 # backbone.maxpool = nn.Identity()
@@ -1314,26 +1318,55 @@ class VisionBackboneBuilder(NetworkBuilder):
                     padding=1,      # Add padding to preserve dimensions after convolution
                     bias=True # False
                 )
-
+            elif backbone_type == 'efficientnet_v2_s':
+                backbone = models.efficientnet_v2_s(pretrained=pretrained)
+                backbone.features[0][0] = nn.Conv2d(input_shape[0], 24, kernel_size=3, stride=1, padding=1, bias=False)
+                backbone_output_size = backbone.classifier[1].in_features
+                backbone.classifier = nn.Identity()
             elif backbone_type == 'vit_b_16':
                 backbone = models.vision_transformer.vit_b_16(pretrained=pretrained)
 
+                # Add a resize layer to ensure the input is correctly sized for ViT
+                resize_layer = nn.Upsample(size=(224, 224), mode='bilinear', align_corners=False)
+
                 backbone_output_size = backbone.heads.head.in_features
                 backbone.heads.head = nn.Identity()
+
+                # Combine the resize layer and the backbone into a sequential model
+                backbone = nn.Sequential(resize_layer, backbone)
+                # # Assuming your input image is a tensor or PIL image, resize it to 224x224
+                # #obs = self.resize_transform(obs)
+                # backbone = models.vision_transformer.vit_b_16(pretrained=pretrained)
+
+                # backbone_output_size = backbone.heads.head.in_features
+                # backbone.heads.head = nn.Identity()
             else:
                 raise ValueError(f'Unknown backbone type: {backbone_type}')
 
             # Optionally freeze the follow-up layers, leaving the first convolutional layer unfrozen
             if backbone_params.get('freeze', False):
+                print('Freezing backbone')
                 for name, param in backbone.named_parameters():
                     if 'conv1' not in name:  # Ensure the first conv layer is not frozen
                         param.requires_grad = False
 
             return backbone, backbone_output_size
 
+        def is_separate_critic(self):
+            return False
+
+        def is_rnn(self):
+            return self.has_rnn
+
+        def get_default_rnn_state(self):
+            num_layers = self.rnn_layers
+            if self.rnn_name == 'lstm':
+                return (torch.zeros((num_layers, self.num_seqs, self.rnn_units)),
+                            torch.zeros((num_layers, self.num_seqs, self.rnn_units)))
+            else:
+                return (torch.zeros((num_layers, self.num_seqs, self.rnn_units)))
+
     def build(self, name, **kwargs):
-        print("Building Network")
-        print(self.params)
         net = VisionBackboneBuilder.Network(self.params, **kwargs)
         return net
 
